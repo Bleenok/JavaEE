8a1f1d7419ea2ac981c5d78f42c230e1517d6dcf
Bleenok <35565019+Bleenok@users.noreply.github.com>
2019-04-10 18:39:08 +0300

    Add files via upload

---
+++ EE_Practice_4/src/main/java/G580.java
@@ -0,0 +1,27 @@
+package com.AspectJ.aspectj;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class G580 {
+    @Autowired
+    @Qualifier("decent")
+    Battery battery;
+
+    @Autowired
+    @Qualifier("4k")
+    Screen screen;
+
+    @Autowired
+    @Qualifier("Intel")
+    GPU cpu;
+
+    String name;
+
+    public G580(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + " \n" + battery.batteryInfo() + screen.screenInfo() + cpu.cpuInfo();
+    }
+}
---
+++ EE_Practice_4/src/main/java/K10.java
@@ -0,0 +1,28 @@
+package com.AspectJ.aspectj;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class K10 {
+    @Autowired
+    @Qualifier("large")
+    Battery battery;
+
+    @Autowired
+    @Qualifier("2k")
+    Display display;
+
+    @Autowired
+    @Qualifier("AMD")
+    GPU cpu;
+
+    String name;
+
+    public K10(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + "\n" +battery.batteryInfo() + screen.screenInfo() + cpu.cpuInfo();
+
+    }
+}
